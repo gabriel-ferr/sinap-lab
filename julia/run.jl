8e4830a07bca4648b0871dcf00347a4b96515e80
Gabriel Ferreira <83837463+gabriel-ferr@users.noreply.github.com>
2023-10-23 21:02:56 -0300

    Adiciona a importação do arquivo CSV escolhido e as configurações do filtros que serão aplicados.

--- julia/run.jl
+++ julia/run.jl
@@ -9,13 +9,11 @@
 #       1 - arquivo .BSON que contém o modelo treinado da rede neural utilizada durante
 #   o programa.
 #
-#       2 - diretório dos arquivos de dados .CSV nomeados como: <ID_IND>_<TASK>.csv,
-#   onde <ID_IND> é o número de identificação do indivíduo que particiou do teste e
-#   <TASK> o número de identificação da tarefa realizada.
+#       2 - arquivo .CSV contendo os dados de uma tarefa.
 #
 #   - 23/10/2023
 #       Executa a estrutura a partir de uma pasta, carregando os arquivos presentes nela,
-#   filtrando e extraindo dados. Esses dados por exportados para arquivos .CSV
+#   filtrando e extraindo dados. Esses dados são exportados para arquivos .CSV
 #   em uma pasta determinada previamente.
 
 #   Referências utilizadas.
@@ -26,4 +24,35 @@ using DataFrames;
 using CSV;
 using BSON: @load;
 
-#   Pega os arquivos que devem ser tratados.
+#   Importa o modelo a ser usado para a rede neural.
+@load ARGS[1] model;
+
+#   Filtros aplicados.
+#   - Filtro notch para remover ruídos da rede/de ambiente.
+fil_notch = Filters.iirnotch(65, 45; fs = 250.0)
+
+#   - Método de aplicação do filtro.
+fil_method = Butterworth(4)
+
+#   - Filtros passa-alta e passa-baixa para onda alfa.
+filt_lowpass_alpha = Lowpass(12; fs = 250.0)
+filt_highpass_alpha = Highpass(8; fs = 250.0)
+
+#   - Filtros passa-alta e passa-baixa para onda beta.
+filt_lowpass_beta = Lowpass(30; fs = 250.0)
+filt_highpass_beta = Highpass(12; fs = 250.0)
+
+#   - Filtros passa-alta e passa-baixa para onda gamma.
+filt_lowpass_gamma = Lowpass(70; fs = 250.0)
+filt_highpass_gamma = Highpass(30; fs = 250.0)
+
+#   Lê o arquivo de dados.
+raw_data = CSV.read(ARGS[2], DataFrame);
+
+#   Processa inicialmente os dados por canal.
+for ch = 1:size(raw_data)[2]
+    #   Pega o valor do canal.
+    raw_channel = raw_data[:, ch]
+
+
+end
